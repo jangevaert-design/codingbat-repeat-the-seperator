a94572d7b72194c8ac5d539340d67c18ec7472c6
Jan Gevaert <jan.gevaert@gmail.com>
2021-06-19 10:11:10 -0600

    Added JUnit5 testing. Runs green.

--- Test/edu/cnm/deepdive/CodingBatRepeatTheSeperatorTest.java
+++ Test/edu/cnm/deepdive/CodingBatRepeatTheSeperatorTest.java
@@ -2,6 +2,50 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class CodingBatRepeatTheSeperatorTest {
+
+  private String[][] stringParams = {
+      {"Word", "X"},
+      {"This", "And"},
+      {"This", "And"},
+      {"Hi", "-n-"},
+      {"AAA", ""},
+      {"AAA", ""},
+      {"A", "B"},
+      {"abc", "XX"},
+      {"abc", "XX"},
+      {"abc", "XX"},
+      {"XYZ", "a"}
+  };
+
+  private int[] countParams = {3, 2, 1, 2, 1, 0, 5, 3, 2, 1, 2};
+
+
+  private String stringExpexted[] = {
+      "WordXWordXWord",
+      "ThisAndThis",
+      "This",
+      "Hi-n-Hi",
+      "AAA",
+      "",
+      "ABABABABA",
+      "abcXXabcXXabc",
+      "abcXXabc",
+      "abc",
+      "XYZaXYZ"
+  };
+
+
+  @Test
+  void test() {
+    for (int i = 0; i < stringParams.length ; i++) {
+      String actual = CodingBatRepeatTheSeperator.repeatSeparator(stringParams[i][0],
+          stringParams[i][1], countParams[i]);
+      Assertions.assertEquals(actual, stringExpexted[i]);
+    }
+  }
 
 }
